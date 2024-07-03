1983e36dae44372d78f31c24823d91d5caa48bdb
John Håvard Wraalsen <John.Havard.Wraalsen@avinor.no>
2024-07-03 12:49:02 +0200

    Converting our HTML to Go Templates, and Creating Handlers : Creating a handler that return JSON

--- 15-bookings/pkg/handlers/handlers.go
+++ 15-bookings/pkg/handlers/handlers.go
@@ -1,10 +1,12 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/johnwr-response/golang-build-modern-web-applications/15-bookings/pkg/config"
 	"github.com/johnwr-response/golang-build-modern-web-applications/15-bookings/pkg/models"
 	"github.com/johnwr-response/golang-build-modern-web-applications/15-bookings/pkg/render"
+	"log"
 	"net/http"
 )
 
@@ -75,4 +77,24 @@ func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
 	start := r.Form.Get("start")
 	end := r.Form.Get("end")
 	w.Write([]byte(fmt.Sprintf("start date is %s and end date is %s", start, end)))
+}
+
+type jsonResponse struct {
+	OK      bool   `json:"ok"`
+	Message string `json:"message"`
+}
+
+// AvailabilityJSON handles requests for availability and send JSON response
+func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
+	resp := jsonResponse{
+		OK:      true,
+		Message: "Available!",
+	}
+	out, err := json.MarshalIndent(resp, "", "     ")
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println(string(out))
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(out)
 }
